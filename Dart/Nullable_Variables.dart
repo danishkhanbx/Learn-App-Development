3ef3cb3229c6166cb18c18eda002594945dfadb2
Danish Khan <83022701+danishkhanbx@users.noreply.github.com>
2024-07-23 23:00:08 +0530

    Create Nullable_Variables.dart

---
+++ Dart/Nullable_Variables.dart
@@ -0,0 +1,29 @@
+// nullable function argument
+void openSocket(int? port){
+  // port can be null
+}
+
+// nullable return type
+String? lastName(String fullName){
+  final components = fullName.split(' ');
+  return components.length > 1 ? components.last : null;
+}
+
+// using parameters
+T? firstNonNull<T>(List<T?> items){
+  // returns first non null element in list if any
+  return items.firstWhere( (item) => item != null);
+} 
+
+void main() {
+  String? name;
+  int? age = 36;
+  age = null;
+  print(lastName('Danish'));
+  print(lastName('Danish Khan'));
+}
+
+
+
+
+
