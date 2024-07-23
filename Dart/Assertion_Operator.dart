3974a99ff24eb9d72dc5f62b8cf4f6c41d95846b
Danish Khan <83022701+danishkhanbx@users.noreply.github.com>
2024-07-23 23:16:51 +0530

    Update Assertion_Operator.dart

--- Dart/Assertion_Operator.dart
+++ Dart/Assertion_Operator.dart
@@ -9,6 +9,10 @@ void main() {
   print(age!);  // good to do
   int? height = null;
   print(height!); // bad practice
+
+  int? maybeValue = 42;
+  int value = maybeValue!; // valid, value is non-nullable
+  // By doing this, we're telling Dart that maybeValue is not null, and it's safe to assign it to a non-nullable variable.
   
   String last1 = lastName('Danish Khan')!;
   String? last2 = lastName('Danish Khan');
