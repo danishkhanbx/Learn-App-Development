6424910d1e0e1341af1b0d99553a547c93cc96d1
Danish Khan <83022701+danishkhanbx@users.noreply.github.com>
2024-07-23 23:23:44 +0530

    Create Non-nullable_variables_Classes.dart file

---
+++ Dart/Non-nullable_variables_Classes.dart
@@ -0,0 +1,11 @@
+class BaseUrl{
+  // String hostName;  // Non-nullable instance field 'hostName' must be initialized
+  int port = 80;  // Good
+  
+  BaseUrl(this.hostName);
+  String hostName;  // now valid
+}
+
+void main(){
+  
+}
